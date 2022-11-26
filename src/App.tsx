244bb5e41eba9e1eaf3616fab3624cd8cb9f6be8
Alex Wilmer <djazium@gmail.com>
2022-11-26 18:31:53 -0500

    It's alive!

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="App">
+      <div>
+        <h1>Michelle Kwong</h1>
+        <h3>Portfolio</h3>
+      </div>
+    </div>
+  );
+}
+
+export default App;
