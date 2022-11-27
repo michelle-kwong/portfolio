5f6628cd3e7773d70d5b7857023a58c03ff3d82b
Michelle Kwong <michellewskwong@gmail.com>
2022-11-27 12:46:09 -0500

    Artwork added.

--- src/App.tsx
+++ src/App.tsx
@@ -69,71 +69,48 @@ export default function App() {
 
 const itemData = [
   {
-    img: "https://images.unsplash.com/photo-1518756131217-31eb79b20e8f",
-    title: "Fern",
+    img: "https://imgur.com/Pkbydys.jpg",
+    title: "嫲嫲"
   },
   {
-    img: "https://images.unsplash.com/photo-1627308595229-7830a5c91f9f",
-    title: "Snacks",
+    img: "https://imgur.com/GFggusN.jpg",
+    title: "exhalation"
   },
   {
-    img: "https://images.unsplash.com/photo-1597645587822-e99fa5d45d25",
-    title: "Mushrooms",
+    img: "https://imgur.com/XFz970l.jpg",
+    title: "heart",
   },
   {
-    img: "https://images.unsplash.com/photo-1529655683826-aba9b3e77383",
-    title: "Tower",
+    img: "https://imgur.com/CS74w36.jpg",
+    title: "notorious",
   },
   {
-    img: "https://images.unsplash.com/photo-1471357674240-e1a485acb3e1",
-    title: "Sea star",
+    img:"https://imgur.com/hXzeqXK.jpg",
+    title: "her",
+  },
+
+  {
+    img: "https://imgur.com/dUWlbQL.jpg",
+    title: "in sickness and in health",
   },
   {
-    img: "https://images.unsplash.com/photo-1558642452-9d2a7deb7f62",
-    title: "Honey",
+    img: "https://imgur.com/xmIldyF.jpg",
+    title: "bare bones",
   },
   {
-    img: "https://images.unsplash.com/photo-1516802273409-68526ee1bdd6",
-    title: "Basketball",
+    img: "https://imgur.com/OAUTbcE.jpg",
+    title: "ethereal",
   },
   {
-    img: "https://images.unsplash.com/photo-1551963831-b3b1ca40c98e",
-    title: "Breakfast",
+    img: "https://imgur.com/p9wzELA.jpg",
+    title: "recovery",
   },
   {
-    img: "https://images.unsplash.com/photo-1627328715728-7bcc1b5db87d",
-    title: "Tree",
+    img: "https://imgur.com/A6HfyCV.jpg",
+    title: "intimacy",
   },
   {
-    img: "https://images.unsplash.com/photo-1551782450-a2132b4ba21d",
-    title: "Burger",
-  },
-  {
-    img: "https://images.unsplash.com/photo-1522770179533-24471fcdba45",
-    title: "Camera",
-  },
-  {
-    img: "https://images.unsplash.com/photo-1444418776041-9c7e33cc5a9c",
-    title: "Coffee",
-  },
-  {
-    img: "https://images.unsplash.com/photo-1627000086207-76eabf23aa2e",
-    title: "Camping Car",
-  },
-  {
-    img: "https://images.unsplash.com/photo-1533827432537-70133748f5c8",
-    title: "Hats",
-  },
-  {
-    img: "https://images.unsplash.com/photo-1567306301408-9b74779a11af",
-    title: "Tomato basil",
-  },
-  {
-    img: "https://images.unsplash.com/photo-1627328561499-a3584d4ee4f7",
-    title: "Mountain",
-  },
-  {
-    img: "https://images.unsplash.com/photo-1589118949245-7d38baf380d6",
-    title: "Bike",
+    img: "https://imgur.com/Br6NqTS.jpg",
+    title: "she",
   },
 ];
