5c43b57b3533f474fdc10e52bfc122b32865bd3b
Alex Wilmer <djazium@gmail.com>
2022-11-27 11:56:26 -0500

    image gallery

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,135 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
+import { Box, Modal } from "@mui/material";
+import Masonry from "@mui/lab/Masonry";
 import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0);
+const style = {
+  position: "absolute" as "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 400,
+  bgcolor: "background.paper",
+  outline: "none",
+};
 
+export default function App() {
+  const [open, setOpen] = useState(false);
   return (
-    <div className="App">
-      <div>
-        <h1>Michelle Kwong</h1>
+    <>
+      <Modal
+        open={!!open}
+        onClose={() => setOpen(false)}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={{ ...style }}>
+          <img
+            style={{
+              width: "100%",
+            }}
+            src={open.img}
+          />
+          <Box style={{ color: "black", textAlign: "center" }}>
+            {open.title}
+          </Box>
+        </Box>
+      </Modal>
+      <Box sx={{ width: 500, minHeight: 829 }}>
+        <h2>Michelle Kwong</h2>
         <h3>Portfolio</h3>
-      </div>
-    </div>
+        <hr />
+        <Box sx={{ marginBottom: "14px" }}>&nbsp;</Box>
+        <Masonry columns={3} spacing={2}>
+          {itemData.map((item, index) => (
+            <div key={index} onClick={() => setOpen(item)}>
+              <img
+                src={`${item.img}?w=162&auto=format`}
+                srcSet={`${item.img}?w=162&auto=format&dpr=2 2x`}
+                alt={item.title}
+                loading="lazy"
+                style={{
+                  borderBottomLeftRadius: 4,
+                  borderBottomRightRadius: 4,
+                  display: "block",
+                  width: "100%",
+                }}
+              />
+            </div>
+          ))}
+        </Masonry>
+      </Box>
+    </>
   );
 }
 
-export default App;
+const itemData = [
+  {
+    img: "https://images.unsplash.com/photo-1518756131217-31eb79b20e8f",
+    title: "Fern",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1627308595229-7830a5c91f9f",
+    title: "Snacks",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1597645587822-e99fa5d45d25",
+    title: "Mushrooms",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1529655683826-aba9b3e77383",
+    title: "Tower",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1471357674240-e1a485acb3e1",
+    title: "Sea star",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1558642452-9d2a7deb7f62",
+    title: "Honey",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1516802273409-68526ee1bdd6",
+    title: "Basketball",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1551963831-b3b1ca40c98e",
+    title: "Breakfast",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1627328715728-7bcc1b5db87d",
+    title: "Tree",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1551782450-a2132b4ba21d",
+    title: "Burger",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1522770179533-24471fcdba45",
+    title: "Camera",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1444418776041-9c7e33cc5a9c",
+    title: "Coffee",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1627000086207-76eabf23aa2e",
+    title: "Camping Car",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1533827432537-70133748f5c8",
+    title: "Hats",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1567306301408-9b74779a11af",
+    title: "Tomato basil",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1627328561499-a3584d4ee4f7",
+    title: "Mountain",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1589118949245-7d38baf380d6",
+    title: "Bike",
+  },
+];
